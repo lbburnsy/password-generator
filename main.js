93a32d6a0bcf9fa1c2e6534e6e7c0128962125f4
Luc Burns <burnsluc2@gmail.com>
2021-02-05 22:16:58 -0500

    Created two functions for boolean return for criteria

--- main.js
+++ main.js
@@ -3,10 +3,39 @@
 let generateButton = document.getElementById("generate");
 let outputEl = document.getElementById("output");
 
-let finalPassword = "asdoifhaosidfas"
+// declare variables
+
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "123456790";
+let specialCharacters = "!@#$%^&*(_)<>?+-";
+
+// Criteria
+
+let wantUpperCase;
+let wantLowerCase;
+let wantNumbers;
+let wantSpecialCharacters;
+
+function returnBoolean(criteria) {
+    criteria = criteria.toLowerCase();
+    if (criteria === "yes") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkCriteria() {
+    wantUpperCase = returnBoolean(prompt("Would you like to include uppercase letters? (Yes/No):"));
+    wantLowerCase = returnBoolean(prompt("Would you like to include lowercase letters? (Yes/No):"));
+    wantNumbers = returnBoolean(prompt("Would you like to include numbers? (Yes/No):"));
+    wantSpecialCharacters = returnBoolean(prompt("Would you like to include special characters? (Yes/No):"));
+}
 
 function writePassword() {
-    outputEl.innerHTML = finalPassword;
+    checkCriteria();
+    // outputEl.innerHTML = finalPassword;
 }
 
 generateButton.addEventListener('click', writePassword);
